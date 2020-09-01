84c3277676e8ebd1a0ef76a2008938c2e93827cd
yepeng <yepeng161517@163.com>
2020-09-01 10:11:19 +0800

    新增用户模块和行情模块

--- STS/STS_eureka/src/main/java/cn/sz/twoOfGroup/STS_Eureka.java
+++ STS/STS_eureka/src/main/java/cn/sz/twoOfGroup/STS_Eureka.java
@@ -3,9 +3,11 @@ package cn.sz.twoOfGroup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaServer
+@ComponentScan("cn.sz.twoOfGroup.config")
 public class STS_Eureka {
     public static void main(String[] args) {
         SpringApplication.run(STS_Eureka.class,args);
